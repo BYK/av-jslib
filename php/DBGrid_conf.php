1685ba3ad135828ec5de417e15ca670109893a78
snlzkn <snlzkn@localhost>
2009-05-04 21:13:22 +0000

    * DBGrid Basic Demo updated.

--- php/DBGrid_conf.php
+++ php/DBGrid_conf.php
@@ -1,13 +1,13 @@
 <?php
 	$columnSets['test']['Basic'] = array('tableName'=>'Test Table', 'columns'=> array (
-		'Name','Population'
+		'ID','Name'
 	));
 	$columnSets['test']['Detailed'] = array('tableName'=>'Test Table', 'columns'=> array (
 		'ID','Name','CountryCode','Population'
 	));
 	$columnSets['test']['allFields']=array(
-		'ID'=>array('visible'=>'0','dontSum'=>'1'),'Name'=>array('alias'=>'City Name','type'=>'string','visible'=>'1','dontSum'=>'1'),
-		'CountryCode'=>array('alias'=>'Country Code','type'=>'string'),'Population'=>array('type'=>'real','dontSum'=>'0')
+		'ID'=>array('visible'=>'0','dontSum'=>'1'),'Name'=>array('title'=>'City Name','type'=>'string','visible'=>'1','dontSum'=>'1'),
+		'CountryCode'=>array('title'=>'Country Code','type'=>'string'),'Population'=>array('type'=>'real','dontSum'=>'0')
 	);
 	$GLOBALS['DBGridColumnSets']=$columnSets;
 	$GLOBALS['DBGridSettings']['exports']=array(

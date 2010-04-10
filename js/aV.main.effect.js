b85696b09dba0fa2926ca61b54cdada845338d50
BYK <BYK@localhost>
2010-04-11 01:13:57 +0300

    Renamed old aV.main.visual.js to aV.main.effect.js aV.Events.add now always returns the event handler given to the function. Fixed page start index problem on DBGrid occuring with dynamic updates. Fixed infoBox does not show up since the created effect is not instructed to start. Standardised the "fade" effect type to comply with other types and made necessary changes to the demo file. Done some style standadisation on aV.plg.quickEdit.js

---
+++ js/aV.main.effect.js
@@ -0,0 +1,286 @@
+/**
+ * @fileOverview A library which introduces an easy to use Effect class
+ * @name Effects Library
+ *
+ * @author Burak Yiğit KAYA <byk@ampliovitam.com>
+ * @version 2.1
+ *
+ * @requires aV.ext.object
+ * @requires aV.main.CSS
+ * @copyright &copy;2010 amplio-Vita under <a href="../license.txt" target="_blank">Apache License, Version 2.0</a>
+ */
+
+if (!aV)
+	throw new Error("aV namespace cannot be found.", "aV.main.effect.js@" + window.location.href);
+
+if (!aV.config.Effect)
+	aV.config.Effect = {};
+	
+aV.config.Effect.unite(
+	{
+		defaults: 
+		{
+			interval: 50,
+			duration: 750,
+			converger: 'exponential'
+		},
+		convergers: 
+		{
+			linear: function(step, steps)
+			{
+				return step / steps;
+			},
+			exponential: function(step, steps)
+			{
+				return 1 - Math.exp(-5 * step / steps);
+			},
+			trigonometric: function(step, steps)
+			{
+				return (Math.cos((step / steps - 1) * Math.PI) + 1) / 2;
+			},
+			power: function(step, steps)
+			{
+				var x = 12 * step / steps;
+				return 1 - Math.pow(x / 2, -x / 4);
+			}
+		},
+		animations: 
+		{
+			fade: function(element, options)
+			{
+				var pattern = /^([.\d]+_|)([.\d]+)/, matchInfo, startValue;
+				matchInfo = pattern.exec(options.value);
+				startValue = parseFloat(matchInfo[1]) || aV.CSS.getOpacity(element) || 0;
+				
+				return [
+					{
+						start: startValue,
+						end: parseFloat(matchInfo[2]) || (startValue < 0.5) ? 1 : 0,
+						set: aV.CSS.setOpacity,
+						converger: options.converger
+					}
+				];
+			},
+			style: function(element, options)
+			{
+				var animations = [], pattern = /^(-?[.\d]+_|)(-?[.\d]+|\*)(pt|px|em|\%|)$/, currentValue, inputMatch, currentMatch;
+				for (var propertyName in options)
+					if (options.hasOwnProperty(propertyName) && propertyName != 'converger')
+					{
+						currentValue = aV.CSS.getElementStyle(element, propertyName);
+						if (currentValue == 'auto') //IE returns auto for unset width and height values
+							currentValue = element['client' + propertyName.ucWords()] + "px";
+						currentMatch = pattern.exec(currentValue);
+						inputMatch = pattern.exec(options[propertyName]);
+						animations.push(
+							{
+								start: parseFloat(inputMatch[1] || currentMatch[2]) || 0,
+								end: (inputMatch[2] !== '*') ? parseFloat(inputMatch[2]) || 0 : element['scroll' + propertyName.ucWords()], //the last part here is actually only for height and width values
+								propertyName: propertyName.camelize(),
+								unit: inputMatch[3] || currentMatch[3] || '',
+								set: function(element, value){element.style[this.propertyName] = value + this.unit;},
+								converger: options.converger
+							}
+						);
+					}
+				return animations;
+			},
+			color: function(element, options)
+			{
+				var animations = [], pattern = /^(.{3,7}_|)(.{3,7})$/, startColor, endColor, inputMatch;
+				for (var propertyName in options)
+					if (options.hasOwnProperty(propertyName) && propertyName != 'converger')
+					{
+						inputMatch = pattern.exec(options[propertyName]);
+						startColor = aV.Color.RGBtoHSL(
+							aV.Color.StringtoRGB(inputMatch[1].substring(0, inputMatch[1].length - 1)) || aV.Color.StringtoRGB(aV.CSS.getElementStyle(element, propertyName))
+						);
+						endColor = aV.Color.RGBtoHSL(aV.Color.StringtoRGB(inputMatch[2]));
+						//the if statements below are for smoother transitions from/to full black and from/to full white color animations
+						if (endColor.l == 1 || endColor.l == 0)
+						{
+							endColor.h = startColor.h;
+							endColor.s = startColor.s;
+						}
+						else if (startColor.l == 1 || startColor.l == 0)
+						{
+							startColor.h = endColor.h;
+							startColor.s = endColor.s;
+						}
+
+						animations.push(
+							{
+								start: 0,
+								end: 1,
+								startColor: startColor,
+								newColor: {h: 0, s:0, l: 0},
+								colorDiff: {h: endColor.h - startColor.h, s: endColor.s - startColor.s, l: endColor.l - startColor.l},
+								propertyName: propertyName.camelize(),
+								set: function(element, value)
+								{
+									this.newColor.h = this.startColor.h + this.colorDiff.h*value;
+									this.newColor.s = this.startColor.s + this.colorDiff.s*value;
+									this.newColor.l = this.startColor.l + this.colorDiff.l*value;
+									element.style[this.propertyName] = aV.Color.RGBtoString(aV.Color.HSLtoRGB(this.newColor));
+								},
+								converger: options.converger
+							}
+						);
+					}
+				return animations;
+			}
+		},
+		sets:
+		{
+			appear: function(element)
+			{
+				aV.CSS.setOpacity(element, 0);
+				if (element.style.display == 'none')
+					element.style.display = ''; //to remove 'none' if exists
+				return {fade: {value: 1}};
+			}
+		}
+	},
+	false
+);
+
+aV.Effect = function(element, animations, options)
+{
+	if (!element)
+		return false;
+
+	this.element = element;
+	this.options = (options || {}).unite(
+		{
+			interval: aV.config.Effect.defaults.interval,
+			duration: aV.config.Effect.defaults.duration,
+			defaultConverger: aV.config.Effect.defaults.converger,
+			ontick: function(){}
+		},
+		false
+	);
+
+	this.step = 0;
+	this.steps = Math.round(this.options.duration / this.options.interval)
+	
+	if (typeof animations == 'string')
+		animations = aV.config.Effect.sets[animations] || {};
+	
+	if (typeof animations == 'function')
+		animations = animations(element);
+		
+	if (animations instanceof Array)
+	{
+		var activeEffect = this;
+		this.animationInitializers = animations[0];
+		for (var i = 1, length = animations.length; i < length; i++)
+			activeEffect = activeEffect.chain(animations[i]);
+		
+		return activeEffect;
+	}
+	else
+	{
+		this.animationInitializers = animations;
+		return this;
+	}
+};
+
+aV.Effect.prototype._progressAnimation = function(index, finished)
+{
+	var animation = this.animations[index];
+	animation.set(this.element, ((finished) ? 1 : animation.converger(this.step, this.steps))*animation.diff + animation.start);
+};
+
+aV.Effect.prototype._progress = function()
+{
+	var finished = ++this.step == this.steps;
+	for (var i = 0, length = this.animations.length; i < length; i++) 
+		this._progressAnimation(i, finished);
+	this.options.ontick(this);
+	if (finished)
+		this.stop();
+	return this;
+};
+
+aV.Effect.prototype.start = function()
+{
+	if (this.baseEffect)
+	{
+		var effect = this.baseEffect;
+		this.baseEffect = undefined;
+		return effect.start();
+	}
+	
+	this.animations = [];
+	var animations = this.animationInitializers,
+	start, animationName, animationSet, animation;
+
+	for (var animationName in animations)
+		if (animations.hasOwnProperty(animationName) && (animationName in aV.config.Effect.animations)) 
+		{
+			animationSet = aV.config.Effect.animations[animationName](this.element, animations[animationName]);
+			for (var i = 0, length = animationSet.length; i < length; i++)
+			{
+				animation = animationSet[i];
+
+				animation.start = animation.start || 0;
+				if (typeof animation.end != 'number')
+					animation.end = 1;
+				animation.diff = animation.end - animation.start;
+				if (!(animation.converger instanceof Function))
+					animation.converger = aV.config.Effect.convergers[animation.converger] || aV.config.Effect.convergers[this.options.defaultConverger];
+				this.animations.push(animation);
+			}
+		}
+	
+	this.resume();
+	return this;
+};
+	
+aV.Effect.prototype.stop = function(stopAll)
+{
+	if (this.ticker) 
+	{
+		window.clearInterval(this.ticker);
+		this.ticker = undefined;
+		if (this.options.onfinish instanceof Function)
+			this.options.onfinish(this);
+		if (!stopAll && this.chainEffect)
+			this.chainEffect.start();
+	}
+	else if (stopAll && this.chainEffect)
+		this.chainEffect.stop(true);
+	return this;
+};
+
+aV.Effect.prototype.resume = function()
+{
+	if (this.activeEffect) 
+		return this.activeEffect.resume();
+	else 
+	{
+		var self = this;
+		this.ticker = window.setInterval(function() {self._progress()}, this.options.interval);
+		return this;
+	}
+};
+
+aV.Effect.prototype.pause = function()
+{
+	if (this.ticker) 
+	{
+		window.clearInterval(this.ticker);
+		this.ticker = undefined;
+		return this;
+	}
+	else if (this.chainEffect)
+		return this.activeEffect = this.chainEffect.pause();
+};
+
+aV.Effect.prototype.chain = function(animations, options, element)
+{
+	this.chainEffect = new aV.Effect(element || this.element, animations, options || this.options);
+	this.chainEffect.baseEffect = this.baseEffect || this;
+	this.baseEffect = undefined;
+	return this.chainEffect;
+};
